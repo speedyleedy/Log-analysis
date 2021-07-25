db39d1c2db18c60d6379336d67ade80bb356a487
Peter <peter.leed@gmail.com>
2021-07-25 13:45:14 +1000

    changed a test to use hamcrest

--- src/test/java/loganalysis/processor/AccessLogMapperTest.java
+++ src/test/java/loganalysis/processor/AccessLogMapperTest.java
@@ -6,6 +6,9 @@ import org.testng.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccessLogMapperTest {
 
     @Test
@@ -14,15 +17,15 @@ public class AccessLogMapperTest {
 
         AccessLogEntry entry = AccessLogMapper.StringToAccessLog(testValue);
 
-        Assert.assertEquals("50.112.00.11", entry.getIpAddress());
-        Assert.assertEquals("/asset.js", entry.getPath());
+        assertThat(entry.getIpAddress(), is("50.112.00.11"));
+        assertThat(entry.getPath(), is("/asset.js"));
 
         testValue = "50.112.00.44 - admin [11/Jul/2018:17:31:56 +0200] \"POST /faces.js HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6\"";
 
         entry = AccessLogMapper.StringToAccessLog(testValue);
 
-        Assert.assertEquals("50.112.00.44", entry.getIpAddress());
-        Assert.assertEquals("/faces.js", entry.getPath());
+        assertThat(entry.getIpAddress(), is("50.112.00.44"));
+        assertThat(entry.getPath(), is("/faces.js"));
 
     }
 
